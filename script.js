7ccc3d8fb172185b95a789f1379ffdf742cc1122
Joel <h23josan@du.se>
2024-11-28 18:22:57 +0100

    Co-authored-by: curoxter <curoxter@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,12 +4,44 @@ console.log(divStyle)
 const textfields = document.getElementsByClassName('textfield')
 console.log(textfields)
 
-const saving = document.getElementById('saveBtn')
+const saving = document.getElementById('delBtn')
 console.log(saving)
 
-const voidDiv = document.getElementById('emptyDiv')
-console.log(voidDiv)
+const emptyDiv = document.getElementById('emptyDiv')
+console.log(emptyDiv)
+
 
 function inputInfo(e) {
+    
     console.log('User: ', e.target);
+    const targetName = e.target.name;
+    if (targetName === 'content') {
+        const inputValue = e.target.value;
+        emptyDiv.innerHTML = inputValue;
+    } else if (targetName === 'Color') { 
+        const colorInput = e.target.value;
+        emptyDiv.style.background = colorInput;
+        emptyDiv.innerHTML = colorInput;
+    } 
+};
+
+for (let i = 0; i < textfields.length; i++) {
+    textfields[1].addEventListener('blur', inputInfo)
 }
+
+function handleclick(e){
+    e.preventDefault();
+    emptyDiv.remove();
+};
+saving.addEventListener('click', handleclick);
+
+
+divStyle.forEach(checkbox => checkbox.addEventListener('change', function(e) {
+    const targetName = e.target.name
+    if (checkbox.checked) {
+    colorInput = e.target.value;
+
+    }
+//.insertAdjacentElement("beforeend", )
+    
+}));
